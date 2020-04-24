9f0fdec7101687cddbab06d70079650aad64a333
kenjn04 <kenjn04@gmail.com>
2020-04-25 00:01:07 +0900

    add drawable animation

--- app/src/main/java/com/sample/myapplication/MainActivity.kt
+++ app/src/main/java/com/sample/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.sample.myapplication
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
 import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
@@ -32,5 +35,14 @@ class MainActivity : AppCompatActivity() {
             icon.isEnabled = false
             icon.isSelected = false
         }
+
+        (anim.drawable as AnimationDrawable).start()
+
+/*
+        val handler = Handler()
+        handler.postDelayed({
+            (anim.drawable as AnimationDrawable).stop()
+        }, 5000)
+ */
     }
 }
